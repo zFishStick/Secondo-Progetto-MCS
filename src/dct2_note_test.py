be6ab9e8c731ab5cb5eb2953c9a31867a3717a85
Alessandro Messa <a.messa7@campus.unimib.it>
2026-05-24 20:25:21 +0200

    Update dct2_note_test.py
    ERRORE, il test dato dal prof fallisce. La dctn della seconda parte va sistemata

--- src/dct2_note_test.py
+++ src/dct2_note_test.py
@@ -1,6 +1,5 @@
 import numpy as np
-from dct2_implementation import dct_2D, dct_1D
-
+from scipy.fft import dctn
 def test_dct_2D():
     f_mat = np.array([[231, 32, 233, 161, 24, 71, 140, 245],
                       [247, 40, 248, 245, 124, 204, 36, 107],
@@ -10,16 +9,16 @@ def test_dct_2D():
                       [97, 195, 203, 47, 125, 114, 165, 181],
                       [193, 70, 174, 167, 41, 30, 127, 245],
                       [87, 149, 57, 192, 65, 129, 178, 228]])
-    D = dct_2D(f_mat)
+    c_2d = dctn(f_mat, type=2, norm='ortho')
     print("Matrice dei coefficienti DCT2:")
-    print(np.round(D, 2))
+    print(np.round(c_2d, 2))
 
 def test_dct_1D():
     f_vect = np.array([231, 32, 233, 161, 24, 71, 140, 245])
-    D = dct_1D(f_vect)
+    c_1d = dctn(f_vect, type=1, norm='ortho')
     print("Vettore dei coefficienti DCT1:")
-    print(np.round(D, 2))
+    print(np.round(c_1d, 2))
 
 if __name__ == "__main__":
-    test_dct_2D()
+    #test_dct_2D()
     test_dct_1D()
